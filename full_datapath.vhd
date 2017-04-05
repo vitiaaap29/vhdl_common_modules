01151333ec69bd00ab2324db546b9c2636862859
xever117 <xever117@gmail.com>
2017-04-06 00:05:27 +0300

    Introduce real datapath module

---
+++ full_datapath.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    00:02:14 04/06/2017 
+-- Design Name: 
+-- Module Name:    full_datapath - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity full_datapath is
+end full_datapath;
+
+architecture struct of full_datapath is
+
+begin
+
+
+end Behavioral;
+
