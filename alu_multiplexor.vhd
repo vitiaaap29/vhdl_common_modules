0878f382acc32e7f1808c0adfa1467a1966a841c
xever117 <xever117@gmail.com>
2017-04-06 10:04:06 +0300

    Implement ALU multiplexor

--- alu_multiplexor.vhd
+++ alu_multiplexor.vhd
@@ -20,22 +20,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity alu_multiplexor is
+    Port ( SEL : in  STD_LOGIC;
+           A   : in  STD_LOGIC_VECTOR (7 downto 0);
+           B   : in  STD_LOGIC_VECTOR (7 downto 0);
+           X   : out STD_LOGIC_VECTOR (7 downto 0));
 end alu_multiplexor;
 
 architecture Behavioral of alu_multiplexor is
-
 begin
-
-
+    X <= A when (SEL = '1') else B;
 end Behavioral;
-
