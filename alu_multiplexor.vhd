bf0dae4a658720bf0ccc737a8b3a386b200d5ef6
xever117 <xever117@gmail.com>
2017-04-06 10:02:36 +0300

    Introduce ALU multiplexor

---
+++ alu_multiplexor.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    09:59:36 04/06/2017 
+-- Design Name: 
+-- Module Name:    alu_multiplexor - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity alu_multiplexor is
+end alu_multiplexor;
+
+architecture Behavioral of alu_multiplexor is
+
+begin
+
+
+end Behavioral;
+
